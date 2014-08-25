59d43723344da8f3472f9be313579481f90e8ddb
Silviu Rosu <silviu.rosu@gmail.com>
2014-08-25 19:02:52 +0200

    change url to https to do not get redirect response

--- lib/facebook_wall/methods.rb
+++ lib/facebook_wall/methods.rb
@@ -5,7 +5,7 @@ module FacebookWall
   #=== Returns
   #String
   def self.feed_url(id)
-    "http://www.facebook.com/feeds/page.php?id=#{id.to_s}&format=rss20"
+    "https://www.facebook.com/feeds/page.php?id=#{id.to_s}&format=rss20"
   end
 
   #Returns the most recent posts from the specified Facebook page's wall.
@@ -16,6 +16,6 @@ module FacebookWall
   def self.posts_by(id)
     feed = RSS::Parser.fetch_and_parse feed_url id
     feed_entry_filter_chain = Factories::create_feed_entry_filter_chain
-    Factories::create_posts feed, feed_entry_filter_chain 
+    Factories::create_posts feed, feed_entry_filter_chain
   end
 end
